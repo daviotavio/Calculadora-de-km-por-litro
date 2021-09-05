dcfe22252cfc291de830cc750e233d90b17dc349
Davi Otavio <85647007+daviotavio@users.noreply.github.com>
2021-09-04 21:34:44 -0300

    Add files via upload

---
+++ km por litro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace km_por_litro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kmPorLitro = 12;
+            int distancia;
+            int quantidade;
+
+            Console.Write("Qual a distância que você irá percorrer? ");
+            distancia = int.Parse(Console.ReadLine());
+            //distancia = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Seu carro faz " +kmPorLitro +" km por litro de gasolina, e ");
+            
+            quantidade = distancia / kmPorLitro;
+
+            Console.Write("a quantidade de gasolina necessária é: " +quantidade+ " litros.");
+        }
+    }
+}
